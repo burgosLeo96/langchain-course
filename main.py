d2a094ba47cc95ecb725ef882a676c6fea82e8ee
Leonard Burgos <burgosleonard96.lb@gmail.com>
2025-11-20 19:47:12 -0500

    Defines model output format.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage
@@ -6,9 +9,22 @@ from langchain_tavily import TavilySearch
 
 load_dotenv()
 
+class Source(BaseModel):
+    """Schema for a source used by the agent."""
+
+    url:str = Field(description="The URL of the source.")
+
+
+class AgentResponse(BaseModel):
+    """Schema for the agent's response."""
+
+    answer: str = Field(description="The final answer provided by the agent.")
+    sources: List[Source] = Field(default_factory=List, description="List of sources used to generate the answer.")
+
+
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
